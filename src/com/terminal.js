f0c16be2772f36ac8ea9703f35f1be83de4df54a
mianumer116 <128652547+mianumer116@users.noreply.github.com>
2024-03-09 17:10:55 +0500

    commits

--- src/com/terminal.js
+++ src/com/terminal.js
@@ -2,11 +2,15 @@
 import './terminal.css';
 
 function Terminal() {
-
+const col={
+  border:"solid",
+  borderColor:'white',
+  marginTop:'45px'
+}
 
   return (
     <>
-      <div className='bla'>
+      <div className='bla' style={col}>
         <h1 className='bdg'>---Terminal</h1>
         <div className="grid lg:grid-cols-1 ba">
           <div className="index_consoleCtrl__DI2H1 index_close__XGN9U"></div>
