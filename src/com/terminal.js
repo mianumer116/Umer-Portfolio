8d62e6e2de8301da7eca99337d156b23d4ef1df0
mianumer116 <128652547+mianumer116@users.noreply.github.com>
2024-03-09 17:04:56 +0500

    commits

--- src/com/terminal.js
+++ src/com/terminal.js
@@ -17,11 +17,11 @@ function Terminal() {
               |
             </span>
           </p>
-          <div className=""></div>
-         
+          
+
         </div>
       </div>
-      
+
     </>
   );
 }
